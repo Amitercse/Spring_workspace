485c569d5daaf046d4fa2c5ebfa73fcb23f0a900
Amit Sharma <amiter.cs@gmail.com>
2021-02-03 13:23:29 +0530

    Test cases for Spring Rest

--- SpringAndRest/src/com/amit/test/TestMessageController.java
+++ SpringAndRest/src/com/amit/test/TestMessageController.java
@@ -52,4 +52,29 @@ public class TestMessageController {
 		.andExpect(MockMvcResultMatchers.jsonPath("$.id").value(3))
 		.andExpect(MockMvcResultMatchers.jsonPath("$.author").value("author 3"));
 	}
+	
+	@Test
+	public void updateMessage() throws Exception {	
+		String jsonData= "{\"author\" : \"author 2\", \"message\" : \"Updated second message posted by author 2\", \"postedDate\" : \"21-01-2021\"}";
+		mockMVC.perform(MockMvcRequestBuilders.put("/messages/{id}", 2).contentType(MediaType.APPLICATION_JSON).content(jsonData)).andExpect(MockMvcResultMatchers.status().isOk());
+		mockMVC.perform(MockMvcRequestBuilders.get("/messages/{id}", 2)).andExpect(MockMvcResultMatchers.status().isOk())
+		.andExpect(MockMvcResultMatchers.jsonPath("$.postedDate").value("21-01-2021"))
+		.andExpect(MockMvcResultMatchers.jsonPath("$.message").value("Updated second message posted by author 2"));
+	}
+	
+	@Test
+	public void testDeleteMessage() throws Exception {
+		mockMVC.perform(MockMvcRequestBuilders.delete("/messages/{id}", 1))
+				.andExpect(MockMvcResultMatchers.status().isOk());
+		mockMVC.perform(MockMvcRequestBuilders.get("/messages")).andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.jsonPath("$.length()").value(2));
+	}
+	
+	@Test
+	public void getXMLMessageById() throws Exception {
+		mockMVC.perform(MockMvcRequestBuilders.get("/messages/{id}", 1).accept(MediaType.APPLICATION_XML))
+				.andExpect(MockMvcResultMatchers.status().isOk())
+				.andExpect(MockMvcResultMatchers.xpath("/message/author", "%s").string("author 1"))
+				.andExpect(MockMvcResultMatchers.xpath("/message/postedDate", "%s").string("2021-01-01"));
+	}
 }
