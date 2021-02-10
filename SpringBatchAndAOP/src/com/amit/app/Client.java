5b924a51a967b49fbf352585995f0a7eb741ae89
Amit Sharma <amiter.cs@gmail.com>
2021-02-10 06:49:45 +0530

    minor refactoring

--- SpringBatchAndAOP/src/com/amit/app/Client.java
+++ SpringBatchAndAOP/src/com/amit/app/Client.java
@@ -5,30 +5,40 @@ import org.springframework.batch.core.Job;
 import org.springframework.batch.core.JobExecution;
 import org.springframework.batch.core.JobParameters;
 import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
 import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Client {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException {
 		ApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 		JobLauncher jobLauncher = (JobLauncher) context.getBean("jobLauncher");
-	//	Job job = (Job) context.getBean("reportJob");
-		try {
-	//		JobExecution execution = jobLauncher.run(job, new JobParameters());
-	//		System.out.println("Exit Status : " + execution.getStatus());
-			Job job = (Job) context.getBean("dbReaderJob");
-			JobParameters params= new JobParametersBuilder().addString("dummyParam", "test val").toJobParameters();
-			JobExecution execution = jobLauncher.run(job, params);
-			if(execution.getStatus()== BatchStatus.FAILED)
-			{
-				System.out.println("Retrying the job");
-				jobLauncher.run(job, params);
-			}
-			System.out.println("Exit Status : " + execution.getStatus());
-		} catch (Exception e) {
-			System.out.println("Error: " + e.getStackTrace());
+	//	runFirstJob(jobLauncher, context);
+		runSecondJob(jobLauncher, context);
+	}
+	
+	private static void runFirstJob(JobLauncher jobLauncher, ApplicationContext context) throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException
+	{
+		Job job = (Job) context.getBean("reportJob");
+		JobExecution execution = jobLauncher.run(job, new JobParameters());
+		System.out.println("Exit Status : " + execution.getStatus());
+	}
+	
+	private static void runSecondJob(JobLauncher jobLauncher, ApplicationContext context) throws JobExecutionAlreadyRunningException, JobRestartException, JobInstanceAlreadyCompleteException, JobParametersInvalidException
+	{
+		Job job2 = (Job) context.getBean("dbReaderJob");
+		JobParameters params= new JobParametersBuilder().addString("dummyParam", "test val").toJobParameters();
+		JobExecution execution2 = jobLauncher.run(job2, params);
+		if(execution2.getStatus()== BatchStatus.FAILED)
+		{
+			System.out.println("Retrying the job");
+			jobLauncher.run(job2, params);
 		}
+		System.out.println("Exit Status : " + execution2.getStatus());
 	}
 }
